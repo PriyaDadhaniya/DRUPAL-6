f95feacaafbd19a1ceed0493cfbfb09c08fe4181
Priya <surejapriya92@gmail.com>
2021-07-05 09:54:13 +0530

    FinShiksha Certi added

---
+++ drupal-6.38-og/ImgtoHtml/index.php
@@ -0,0 +1,82 @@
+<html style="height: 100%;">
+<head>
+<title>Dummy Certificate of FinShiksha</title>
+<style>
+  @page{
+     size:A4 landscape;
+}
+
+body{
+    margin:0;
+    padding:0;
+    align-content: center;
+}
+
+.border{
+    background-image: url('Border.png');
+    color: black;
+    background-size: 808px 625px;
+    padding-top: 100%;
+    background-repeat: no-repeat;
+    position: relative;
+}
+
+.content{
+    position:absolute;
+    text-align: center;
+    left:12mm;
+    top:30mm;
+}
+
+.coloured{
+    background: green;
+    top: 50%;
+    height: 20mm;
+    color: white;
+    left:0mm;
+    padding-left: -30mm;
+    padding-right: 43mm;
+}
+
+.coloured h2{
+    text-align: center;
+    margin-left: 48mm;
+}
+
+.inner-content{
+    padding-top: 10mm;
+    margin-left: 0mm
+}
+
+h3{
+   font-style:  italic;
+   color: darkblue;
+   font-weight: normal;
+}
+.inner-content h1{
+   font-style:  italic;
+   color: midnightblue;
+   font-weight: bold;
+}
+
+</style>
+</head>
+<body>
+<div class="border">
+  <div class="content">
+    <div class="coloured">
+      <h2>CERTIFICATE OF COMPLETION</h2>
+      <h2>(COURSE NAME)</h2>
+   </div>
+   
+   <div class="inner-content">
+      <h3>This Certificate of completion hereby is granted to</h3>
+      <h1> (PARTICIPANT's NAME) </h1>
+      <h3>for successful completion of </h3>
+      <h3>Certification in (Course Name)  </h3>
+      <h3>in  (MONTH) (YEAR) securing (Grade).</h3>
+   </div>
+  </div>
+</div>
+</body>
+</html>
